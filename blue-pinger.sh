452cb62d73bf4131a24a5ce0834e7ee7d41e0b0e
Jayson Cabrillas San Buenaventura <52001009+mkdirlove@users.noreply.github.com>
2020-07-19 12:30:23 +0800

    Update blue-pinger.sh

--- blue-pinger.sh
+++ blue-pinger.sh
@@ -4,6 +4,22 @@
 
 #!/usr/bin/bash
 
+#COLORS
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cyan="\033[0;36m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+blue="\033[1;34m"
+nc="\e[0m"
+bln="\033[33m"
+bc="\033[0m"
+
 # Check if running as root.
 function check_sudo() {
   if [[ "$EUID" -ne 0 ]]; then
